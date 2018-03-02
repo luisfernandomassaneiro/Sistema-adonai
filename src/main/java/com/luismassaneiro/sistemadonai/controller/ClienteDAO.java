5652e94d95060c1a882c03b087c45897d43c9bd6
Massaneiro <Massaneiro@192.168.0.213>
2018-03-02 13:13:38 -0300

    Modelagem do cadastro de clientes

---
+++ src/main/java/com/luismassaneiro/sistemadonai/controller/ClienteDAO.java
@@ -0,0 +1,79 @@
+package com.luismassaneiro.sistemadonai.controller;
+
+import com.luismassaneiro.sistemadonai.exceptions.ValidateException;
+import com.luismassaneiro.sistemadonai.model.Cliente;
+import com.luismassaneiro.sistemadonai.model.Produto;
+import com.luismassaneiro.sistemadonai.utils.TrataExcecao;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ *
+ * @author matheus_peressoni
+ */
+public class ClienteDAO extends GenericDAO<Cliente> {
+    public List<Cliente> recuperaClientesComFiltros(String codigo, String nome, boolean somenteAtivo) throws ValidateException {
+        try {
+            Map<String, Object> parameters = new HashMap<>();
+            StringBuilder hql = new StringBuilder();
+            
+            hql.append("select c from Cliente as c ");
+            if(StringUtils.isNotEmpty(codigo) || StringUtils.isNotEmpty(nome) || somenteAtivo) {
+                hql.append(" where ");
+                if(StringUtils.isNotEmpty(codigo)) {
+                    hql.append(" c.codigo like :codigo ");
+                    parameters.put("codigo", codigo);
+                    hql.append(StringUtils.isNotEmpty(nome) || somenteAtivo ? " and " : "");
+                }
+                if(StringUtils.isNotEmpty(nome)) {
+                    hql.append(" c.nome like :nome ");
+                    parameters.put("nome", nome);
+                    hql.append(somenteAtivo ? " and " : "");
+                }
+                if(somenteAtivo) {
+                    hql.append(" c.ativo = 1 ");
+                }
+            }
+            hql.append(" order by p.nome ");
+            return list(hql.toString(), parameters);
+        } catch (Exception e) {
+            String erro = TrataExcecao.trataMensagemErro(e, ClienteDAO.class);
+            throw new ValidateException(erro);
+        } 
+    }
+    
+    public Produto recuperaClientePeloCodigo (String codigo) throws ValidateException {
+        try {
+            Map<String, Object> parameters = new HashMap<>();
+            StringBuilder hql = new StringBuilder();
+            
+            hql.append("select c from Cliente as c ");
+            hql.append(" where ");
+            hql.append(" c.codigo = :codigo ");
+            parameters.put("codigo", codigo);
+            return find(hql.toString(), parameters);
+        } catch (Exception e) {
+            String erro = TrataExcecao.trataMensagemErro(e, ClienteDAO.class);
+            throw new ValidateException(erro);
+        } 
+    }
+    
+     public boolean codigoProdutoDisponivel(String codigo) throws ValidateException {
+        try {
+            Map<String, Object> parameters = new HashMap<>();
+            StringBuilder hql = new StringBuilder();
+            hql.append("select count(c.id) from Cliente as c ");
+            hql.append(" where ");
+            hql.append(" c.codigo = :c ");
+            parameters.put("codigo", codigo);
+            Long count = find(hql.toString(), parameters);
+            return count == 0;
+        } catch (Exception e) {
+            String erro = TrataExcecao.trataMensagemErro(e, ProdutoDAO.class);
+            throw new ValidateException(erro);
+        } 
+    }
+}
+
---
+++ src/main/java/com/luismassaneiro/sistemadonai/model/Cliente.java
@@ -0,0 +1,164 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luismassaneiro.sistemadonai.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author matheus_peressoni
+ */
+@Entity
+@Table(name="TB_CLIENTE")
+public class Cliente extends Base implements BusinessEntity{
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="CLI_ID")
+    private Long id;
+    
+    @Column(name="CLI_CODIGO",nullable=false)
+    private String codigo;
+    
+    @Column(name="CLI_NOME",nullable=false)
+    private String nome;    
+
+    @Column(name="CLI_RESIDENCIAL")
+    private String telefoneResidencial;
+    
+    @Column(name="CLI_CELULAR")
+    private String telefoneCelular;
+    
+    @Column(name="CLI_DATANASCIMENTO")
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date dataNascimento;
+    
+    @Column(name="CLI_RUA")
+    private String rua;
+    
+    @Column(name="CLI_NUMERO")
+    private String numero;
+    
+    @Column(name="CLI_CEP")
+    private String cep;
+    
+    @Column(name="CLI_BAIRRO")
+    private String bairro;
+    
+    @Column(name="CLI_CIDADE")
+    private String cidade;
+    
+    @Column(name="CLI_ESTADO")
+    private String estado;
+    
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefoneResidencial() {
+        return telefoneResidencial;
+    }
+
+    public void setTelefoneResidencial(String telefoneResidencial) {
+        this.telefoneResidencial = telefoneResidencial;
+    }
+
+    public String getTelefoneCelular() {
+        return telefoneCelular;
+    }
+
+    public void setTelefoneCelular(String telefoneCelular) {
+        this.telefoneCelular = telefoneCelular;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+}
