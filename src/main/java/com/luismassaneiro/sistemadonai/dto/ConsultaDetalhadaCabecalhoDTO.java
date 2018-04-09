fc43b3a8400dd8ca968c27435abb14d075f4fa34
luis.massaneiro <luis.massaneiro@senior.com.br>
2018-04-09 13:29:55 -0300

    Início da implementação do relatório de extrato de clientes

---
+++ src/main/java/com/luismassaneiro/sistemadonai/dto/ConsultaDetalhadaCabecalhoDTO.java
@@ -0,0 +1,42 @@
+package com.luismassaneiro.sistemadonai.dto;
+
+import java.util.List;
+
+public class ConsultaDetalhadaCabecalhoDTO {
+    private String cliente;
+    private String periodo;
+    private String situacao;
+    private List<ConsultaDetalhadaDetalheDTO> detalhe;
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public List<ConsultaDetalhadaDetalheDTO> getDetalhe() {
+        return detalhe;
+    }
+
+    public void setDetalhe(List<ConsultaDetalhadaDetalheDTO> detalhe) {
+        this.detalhe = detalhe;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+}
